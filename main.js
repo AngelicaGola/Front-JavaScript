06d2a7a1c87c70dad2e512704891fcda13efcd6f
AngelicaGola <maagel52@gmail.com>
2022-11-17 20:54:33 -0500

    Menu Mobile

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const menuMobIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+menuMobIcon.addEventListener('click', toggleMobileMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 
 }
